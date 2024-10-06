47da55aa8d5904642ad1bfa8efbcf3c4dd5da753
christian-amarildo <christianamarildo@hotmail.com>
2024-10-06 00:31:01 -0300

    ajustei os testes e melhorei o codigo

--- Questão 1/laboratorio.py
+++ Questão 1/laboratorio.py
@@ -20,30 +20,33 @@ class AFD:
 
 # a) (ab*c*)*
 automato_a = AFD(
-    estados={'q0', 'q1', 'q2', 'q3'},
+    estados={'q0', 'q1', 'q2'},
     simbolos_entrada={'a', 'b', 'c'},
     transicoes={
-        'q0': {'a': 'q1'},      # a vai para q1
-        'q1': {'b': 'q1', 'c': 'q2'},  # b fica em q1, c vai para q2
-        'q2': {'c': 'q2', 'a': 'q1'},  # c fica em q2, a volta para q1
+        'q0': {'a': 'q1'},        # a vai para q1
+        'q1': {'a': 'q1','b': 'q1', 'c': 'q2'},  # b permanece em q1, c vai para q2
+        'q2': {'c': 'q2', 'a': 'q1'},  # c permanece em q2, a retorna para q1
     },
     estado_inicial='q0',
-    estados_finais={'q0', 'q2'}  # estados finais são q0 e q2
+    estados_finais={'q0', 'q1', 'q2'}  # q0, q1 e q2 são estados finais
 )
 
 # b) aaa(b | c)* | (b | c)* aaa
 automato_b = AFD(
-    estados={'q0', 'q1', 'q2', 'q3', 'q4'},
+    estados={'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8'},
     simbolos_entrada={'a', 'b', 'c'},
     transicoes={
-        'q0': {'a': 'q1'},       # a vai para q1
-        'q1': {'a': 'q2'},       # a vai para q2
-        'q2': {'a': 'q3'},       # a vai para q3
-        'q3': {'b': 'q3', 'c': 'q3'},  # b ou c fica em q3
-        'q4': {'b': 'q4', 'c': 'q4'},  # b ou c fica em q4
+        'q0': {'a': 'q1', 'b': 'q5', 'c': 'q5'},   # q0 pode ir para q1 com 'a', ou para q5 com 'b' ou 'c'
+        'q1': {'a': 'q2'},                         # q1 vai para q2 com 'a'
+        'q2': {'a': 'q3'},                         # q2 vai para q3 com 'a'
+        'q3': {'b': 'q4', 'c': 'q4'},              # q3 vai para q4 com 'b' ou 'c'
+        'q4': {'b': 'q4', 'c': 'q4'},              # q4 permanece em q4 com 'b' ou 'c'
+        'q5': {'b': 'q5', 'c': 'q5', 'a': 'q6'},   # q5 permanece em q5 com 'b' ou 'c', e vai para q6 com 'a'
+        'q6': {'a': 'q7'},                         # q6 vai para q7 com 'a'
+        'q7': {'a': 'q8'},                         # q7 vai para q8 com 'a'
     },
     estado_inicial='q0',
-    estados_finais={'q3', 'q4'}  # estados finais são q3 e q4
+    estados_finais={'q4', 'q8'}  # q4 e q8 são estados finais
 )
 
 # c) a*b | ab*
@@ -52,9 +55,9 @@ automato_c = AFD(
     simbolos_entrada={'a', 'b'},
     transicoes={
         'q0': {'a': 'q1', 'b': 'q2'},  # a vai para q1, b vai para q2
-        'q1': {'a': 'q1', 'b': 'q3'},  # a fica em q1, b vai para q3
-        'q2': {},                       # q2 é final
-        'q3': {'b': 'q3'},             # b fica em q3
+        'q1': {'a': 'q1', 'b': 'q3'},  # a permanece em q1, b vai para q3
+        'q2': {},                       # q2 é um estado final
+        'q3': {'b': 'q3'},             # b permanece em q3
     },
     estado_inicial='q0',
     estados_finais={'q1', 'q2', 'q3'}  # estados finais são q1, q2 e q3
@@ -65,13 +68,15 @@ automato_d = AFD(
     estados={'q0', 'q1', 'q2', 'q3'},
     simbolos_entrada={'a', 'b', 'c'},
     transicoes={
-        'q0': {'a': 'q1'},               # a vai para q1
-        'q1': {'a': 'q1', 'b': 'q2'},    # a fica em q1, b vai para q2
-        'q2': {'a': 'q3'},               # b vai para q2, a vai para q3
-        'q3': {'c': 'q3'},               # a ou c fica em q3
+        'q0': {'a': 'q1','b': 'q2'},               # a vai para q1
+        'q1': {'a': 'q3', 'b': 'q2'},    # a permanece em q1, b vai para q2
+        'q2': {'a': 'q6','b': 'q2'},               # b vai para q2, a vai para q3
+        'q3': {'a': 'q3','c': 'q5'},               # a ou c permanece em q3
+        'q4': {'c': 'q5'},
+        'q5': {'c': 'q5'},
     },
     estado_inicial='q0',
-    estados_finais={'q2', 'q3'}  # estados finais são q2 e q3
+    estados_finais={'q1', 'q3', 'q5', 'q4'}
 )
 
 def testar_automato(automato, strings_teste):
